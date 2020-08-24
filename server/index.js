762aa4bcbc76d4976df2e81e1adeeedb1e0de034
Ana Todorova <todorova@outlook.com>
2020-08-24 22:51:58 +0300

    add gatsby

--- server/index.js
+++ server/index.js
@@ -1,22 +1,34 @@
-const express = require("express");
-const app = express();
+const express = require ('express');
+const app = express ();
 const port = 3000;
-const queries = require('./queries')
+const queries = require ('./queries');
 
-app.get("/election_results", (req, res) => {
-  console.log('election_results', req.query)
-  return queries.election_results({
-    election_id: req.query.election_id,
-    municipality_id: req.query.municipality_id
-  }).then((result) => res.send(result));
+app.get ('/api/election_results', (req, res) => {
+  console.log ('election_results', req.query);
+  return queries
+    .election_results ({
+      election_id: req.query.election_id,
+      municipality_id: req.query.municipality_id,
+    })
+    .then (result => res.send (result));
 });
 
-
-app.get("/elections", (req, res) => {
-  console.log('elections', req.query)
-  return queries.elections(req.query).then((result) => res.send(result));
+app.get ('/api/districts', (req, res) => {
+  return queries.districts (req.query).then (result => res.send (result));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get ('/api/municipalities/:district_id', (req, res) => {
+  return queries
+    .municipalities ({
+      district_id: req.params.district_id,
+    })
+    .then (result => res.send (result));
+});
+
+app.get ('/api/elections', (req, res) => {
+  return queries.elections (req.query).then (result => res.send (result));
+});
+
+app.listen (port, () => {
+  console.log (`Example app listening at http://localhost:${port}`);
 });
